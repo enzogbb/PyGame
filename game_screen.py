f2f220a4171848440883e6d93f81831dca8ebe6d
enzogbb <152035250+enzogbb@users.noreply.github.com>
2024-01-30 10:07:49 -0300

    Update 2.0 'game_screen.py' - Ajuste dos retângulos
    Foi ajustado o posicionamento, largura e altura de cada retângulo, para que então preencham melhor a tela de jogo.

--- game_screen.py
+++ game_screen.py
@@ -7,30 +7,37 @@ def desenhar_retangulos(window, pergunta_atual):
     cor_branca = (255, 255, 255)
     cor_preta = (0, 0, 0)
 
-    # Tamanho dos retângulos
-    largura_retangulo = 300
+    # Ajuste da largura dos retângulos
+    largura_retangulo = 1000
     altura_retangulo = 50
 
-    # Posições dos retângulos
-    pos_x = 50
-    pos_y = 50
+    # Posições iniciais dos retângulos
+    pos_x = (WIDTH - largura_retangulo) // 2
+    pos_y = (HEIGHT - (altura_retangulo + 20) * (len(pergunta_atual['alternativas']) + 1)) // 2
 
-    # Desenha o retângulo da pergunta
-    pygame.draw.rect(window, cor_branca, (pos_x, pos_y, largura_retangulo, altura_retangulo))
+    # Desenha o primeiro retângulo esticado na parte superior centralizada
+    pygame.draw.rect(window, cor_branca, (pos_x, 0, largura_retangulo, altura_retangulo * 3))
 
     # Adiciona o texto da pergunta no retângulo
     fonte = pygame.font.Font(None, 36)
     texto_pergunta = fonte.render(pergunta_atual['enunciado'], True, cor_preta)
-    window.blit(texto_pergunta, (pos_x + 10, pos_y + 10))
+    text_rect = texto_pergunta.get_rect(center=(pos_x + largura_retangulo // 2, altura_retangulo * 1.5))
+    window.blit(texto_pergunta, text_rect)
+
+    # Ajusta a posição vertical para as alternativas
+    pos_y += altura_retangulo + 20  
 
     # Desenha os retângulos das alternativas
     for i, alternativa in enumerate(pergunta_atual['alternativas']):
-        pos_y += altura_retangulo + 20  # Ajusta a posição vertical para a próxima alternativa
         pygame.draw.rect(window, cor_branca, (pos_x, pos_y, largura_retangulo, altura_retangulo))
 
         # Adiciona o texto da alternativa no retângulo
         texto_alternativa = fonte.render(alternativa, True, cor_preta)
-        window.blit(texto_alternativa, (pos_x + 10, pos_y + 10))
+        text_rect = texto_alternativa.get_rect(center=(pos_x + largura_retangulo // 2, pos_y + altura_retangulo // 2))
+        window.blit(texto_alternativa, text_rect)
+
+        # Ajusta a posição vertical para a próxima alternativa
+        pos_y += altura_retangulo + 20  
 
 def game_screen(window):
     # Variável para o ajuste de velocidade
@@ -42,7 +49,7 @@ def game_screen(window):
     PLAYING = 1
     state = PLAYING
 
-        # Pergunta atual
+    # Pergunta atual
     pergunta_atual = perguntas[0]
 
     # ===== Loop principal =====
